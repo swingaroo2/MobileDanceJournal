0228e26769cec15d0e289db8f372aa57c14dad81
Zach Lockett-Streiff <zachls17@gmail.com>
2019-11-10 18:33:35 -0600

    Streamlining ThumbnailCacheTests

--- JiveJournalTests/ThumbnailCacheTests.swift
+++ JiveJournalTests/ThumbnailCacheTests.swift
@@ -12,14 +12,6 @@ import XCTest
 class ThumbnailCacheTests: XCTestCase {
 
     let sut = ThumbnailCache()
-    
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
     func testCacheInteractions() {
         let testImage = UIImage()
